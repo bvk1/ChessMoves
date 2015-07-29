7569c03822ed7fd128ad63f82f28048bddc3837d
Bhavya V. Karki <bhavyakarki@gmail.com>
2015-07-29 12:40:35 +0530

    Skeletal structure for chess piece

---
+++ Piece.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Piece{
+  
+  protected String position;
+  protected boolean isKilled;
+  protected char colourOfPiece;
+  
+  Piece(String position,char color){
+  position = position;
+  isKilled = false;
+  colourOfPiece = color;
+  
+  }
+  protected void moveTo(String position){}
+  protected boolean canMoveTo(String position){}
+  protected String getPosition(Piece obj){}
+  }
