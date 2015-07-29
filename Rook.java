0093d1fbc8b5f764c90e17497872d9c50b4c800c
garvitjain <garvitjain314@gmail.com>
2015-07-29 16:12:58 +0530

    Adding canMoveTo function for rook

--- Rook.java
+++ Rook.java
@@ -5,8 +5,21 @@ public class Rook extends Piece
 		super(position, color, 'R');
 	}
 	
-	public boolean canMoveTo(String position)
-	{
-		
+	public bool canMoveTo(string toPosition, final boolean[][] chessBoard) {
+		String fromPosition = getPosition();
+		if(fromPosition.charAt(0) == toPosition.charAt(0)) {
+			for(int i = min(fromPosition.charAt(1),toPosition.charAt(1)) + 1; i <= max(fromPosition.charAt(1),toPosition.charAt(1)); i++) {
+				if(chessBoard[8-(fromPosition.charAt(0)+'a')][i]) {
+					return false;
+				}
+			}
+		}
+		if(fromPosition.charAt(1) == toPosition.charAt(1)) {
+			for(int i = min(fromPosition.charAt(0),toPosition.charAt(0)) + 1; i <= max(fromPosition.charAt(0),toPosition.charAt(0)); i++) {
+				if(chessBoard[i-'a'][toPosition.charAt(1)]) {
+					return false;
+				}
+			}
+		}
 	}
 }
