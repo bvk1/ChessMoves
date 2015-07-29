dde1d9ed9dca8423ea770510a69674d89a73ecfa
Bhavya V. Karki <bhavyakarki@gmail.com>
2015-07-29 16:16:12 +0530

    bug fixes in pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,6 +1,6 @@
-public class Bishop extends Piece
+public class Pawn extends Piece
 {
-	public King(String position, char color)
+	public Pawn(String position, char color)
 	{
 		super(position, color);
 	}
